1b937da830391fb83194069fda37b0b11e062de9
jiseong0612 <hth5545@gmail.com>
2022-02-11 10:37:50 +0900

    querySelec 테스트(잘된다)

--- typescript/hello/helloTs.js
+++ typescript/hello/helloTs.js
@@ -1,7 +1,9 @@
+var _this = this;
 var $body = document.body;
 var $div = document.createElement('div');
 var $input = document.createElement('input');
 var $btn = document.createElement('button');
+var $selec = document.querySelector('#selec');
 $input.placeholder = 'what\'s your name';
 $btn.textContent = 'click';
 $div.append($input, $btn);
@@ -9,4 +11,8 @@ $body.append($div);
 $btn.addEventListener('click', function () {
     var userName = $input.value;
     alert("my name is ".concat(userName));
+});
+$selec.addEventListener('change', function (event) {
+    console.log(_this); // 화살표 함수에서 this는 window를 가르킴
+    console.log(event.target);
 });
--- typescript/hello/helloTs.ts
+++ typescript/hello/helloTs.ts
@@ -3,6 +3,8 @@ const $div = document.createElement('div');
 const $input = document.createElement('input');
 const $btn = document.createElement('button');
 
+const $selec = document.querySelector('#selec') as HTMLDivElement;
+
 $input.placeholder = 'what\'s your name';
 $btn.textContent = 'click';
 $div.append($input, $btn);
@@ -12,4 +14,10 @@ $btn.addEventListener('click', ()=>{
     let userName:string = $input.value; 
     alert(`my name is ${userName}`);
 });
+
+$selec.addEventListener('change', (event)=>{
+    console.log(this); // 화살표 함수에서 this는 window를 가르킴
+    console.log(event.target);    
+})
+
 
